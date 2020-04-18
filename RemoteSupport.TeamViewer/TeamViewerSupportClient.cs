d1207ad17ebde3fe3c63d24974d0f3b9d19707db
Piotr Gawlicki <piotr.gawlicki@xiron.pl>
2020-04-18 16:36:39 +0200

    Skeleton for TeamViewer

---
+++ RemoteSupport.TeamViewer/TeamViewerSupportClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RemoteSupport.TeamViewer
+{
+    public class TeamViewerSupportClient : IRemoteAccessClient
+    {
+        public bool IsInstalled => throw new NotImplementedException();
+
+        public bool InstalationIsNeeded => throw new NotImplementedException();
+
+        public bool Acticvate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Download(string localPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
