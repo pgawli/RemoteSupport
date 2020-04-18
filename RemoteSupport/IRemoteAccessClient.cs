e0dfe845194f788609306fa17cab9126fb507adf
Piotr Gawlicki <piotr.gawlicki@xiron.pl>
2020-04-18 16:34:24 +0200

    Basic files

---
+++ RemoteSupport/IRemoteAccessClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RemoteSupport
+{
+    public interface IRemoteAccessClient
+    {
+        bool IsInstalled { get; }
+        bool InstalationIsNeeded { get; }
+        bool Download(string localPath);
+        bool Acticvate();
+    }
+}
